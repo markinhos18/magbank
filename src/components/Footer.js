4c46f8a35603bee70701c595d086e9ddbe7f443f
Marcos Felipe <felipe.oliveira.ext@espm.br>
2022-03-07 00:46:21 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Image, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCreditCard, faWallet, faShield, faUserTie } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import Logo from "../assets/logo.svg";
 import './Footer.scss';
 
@@ -9,32 +9,35 @@ const Footer = () => (
     <footer className="footer py-3">
         <Container fluid className="px-5">
             <Row>
-                <Col className="d-flex align-items-center justify-items-center">
+                <Col  className="d-flex align-items-center justify-items-center">
                     <Image src={Logo} width="60"  alt="NuBank logo" />
                 </Col>
-                <Col className="d-flex align-items-center justify-items-center">
-                    <p>© 2021  Nu Pagamentos S.A - Instituição de Pagamento.  18.236.120/0001-58 Rua Capote Valente, 39 - São Paulo, SP - 05409-000</p>
+                <Col className="d-flex align-items-center justify-items-center text-center">
+                    <p>© 2021  Nu Pagamentos S.A - Instituição de Pagamento.  18.236.120/0001-58<br /> Rua Capote Valente, 39 - São Paulo, SP - 05409-000</p>
                 </Col>
                 <Col>
-                    <div class="Social-dxbrx9-0 jwQMJv">
-                        <a class="sc-qZtCU AgwVi sc-oUoif fnZucx StyledLink-sc-126lr5p-0 dZdtGM" aria-label="Navegar para o nosso perfil no Facebook" href="https://www.facebook.com/nubank" target="_blank" rel="noopener">
-                            
+                    <div class="d-flex justify-content-end align-items-center">
+                        <a class="align-self-center col-1" aria-label="Navegar para o nosso perfil no Facebook" href="https://www.facebook.com/nubank" target="_blank" rel="noopener">
+                            <FontAwesomeIcon icon={faFacebook}  size="2x" color="white"/>
                         </a>
                         
-                        <a class="sc-qZtCU AgwVi sc-oUoif fnZucx StyledLink-sc-126lr5p-0 dZdtGM" aria-label="Navegar para o nosso perfil no Instagram" href="https://www.instagram.com/nubank/" target="_blank" rel="noopener">
+                        <a class="align-self-center col-1" aria-label="Navegar para o nosso perfil no Instagram" href="https://www.instagram.com/nubank/" target="_blank" rel="noopener">
+                            <FontAwesomeIcon icon={faInstagram}  size="2x" color="white"/>
                             
                         </a>
 
-                        <a class="sc-qZtCU AgwVi sc-oUoif fnZucx StyledLink-sc-126lr5p-0 dZdtGM" aria-label="Navegar para o nosso perfil no LinkedIn" href="https://www.linkedin.com/company/nubank" target="_blank" rel="noopener">
+                        <a class="align-self-center col-1" aria-label="Navegar para o nosso perfil no LinkedIn" href="https://www.linkedin.com/company/nubank" target="_blank" rel="noopener">
+                            <FontAwesomeIcon icon={faLinkedin}  size="2x" color="white"/>
                            
                         </a>
 
-                        <a class="sc-qZtCU AgwVi sc-oUoif fnZucx StyledLink-sc-126lr5p-0 dZdtGM" aria-label="Navegar para o nosso canal no Youtube" href="https://www.youtube.com/nubank" target="_blank" rel="noopener">
+                        <a class="align-self-center col-1" aria-label="Navegar para o nosso canal no Youtube" href="https://www.youtube.com/nubank" target="_blank" rel="noopener">
+                            <FontAwesomeIcon icon={faYoutube}  size="2x" color="white"/>
                             
                         </a>
 
-                        <a class="sc-qZtCU AgwVi sc-oUoif fnZucx StyledLink-sc-126lr5p-0 dZdtGM" aria-label="Navegar para o nosso perfil no Twitter" href="https://twitter.com/nubank" target="_blank" rel="noopener">
-                            
+                        <a class="align-self-center col-1" aria-label="Navegar para o nosso perfil no Twitter" href="https://twitter.com/nubank" target="_blank" rel="noopener">
+                            <FontAwesomeIcon icon={faTwitter}  size="2x" color="white"/> 
                         </a>
                     </div>
                 </Col>
