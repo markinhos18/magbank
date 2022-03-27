5befab5e2c44c767e32aa0231e98859313bc3325
Marcos Felipe <felipe.oliveira.ext@espm.br>
2022-03-26 21:57:40 -0300

    ajuste FAq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -62,32 +62,50 @@ const Faq = () => {
                             <Accordion.Item eventKey="1">
                                 <Accordion.Header>Conta e abertura</Accordion.Header>
                                 <Accordion.Body>
-                                    A conta do Nubank é a nossa conta digital e, assim como tudo no Nubank, foi desenhada para todos que desejam um serviço transparente, simples e sem tarifas abusivas. Ela foi feita para redefinir a maneira como você cuida do seu dinheiro. Para cumprir esse papel, a conta do Nubank:
+                                    <p>
+                                        A conta do Nubank é a nossa conta digital e, assim como tudo no Nubank, foi desenhada para todos que desejam um serviço transparente, simples e sem tarifas abusivas. Ela foi feita para redefinir a maneira como você cuida do seu dinheiro. Para cumprir esse papel, a conta do Nubank:<br />
 
-                                    — Não tem anuidade, nem taxa de manutenção;
 
-                                    — Tem rendimento superior à poupança diário e automático, desde o dia em que seu dinheiro é depositado. Além disso, todos os depósitos contam com algum tipo de garantia;
 
-                                    — Oferece transferências gratuitas e ilimitadas para qualquer banco;
 
-                                    — Tem um cartão com função débito para você fazer compras e saques;
+                                        — Não tem anuidade, nem taxa de manutenção;<br />
 
-                                    — Possibilita pagamento de boletos como conta de luz, água, telefone e até mesmo compras online.
+                                        — Tem rendimento superior à poupança diário e automático, desde o dia em que seu dinheiro é depositado. Além disso, todos os depósitos contam com algum tipo de garantia;<br />
 
-                                    A função débito está disponível para todos os clientes da conta do Nubank! Os clientes interessados em ter um cartão habilitado com essa função devem pedir diretamente pelo nosso app. Abaixo explicamos melhor esse processo.
+                                        — Oferece transferências gratuitas e ilimitadas para qualquer banco;<br />
 
-                                    Para quem já possui o novo modelo de cartão de crédito Nubank, lançado em setembro de 2018, basta entrar na área da sua Conta, clicar em 'Pedir função débito' e pronto! Se preferir, pode também arrastar sua tela inicial para cima até a sessão "Descubra Mais" e clicar na opção desejada. Seu cartão já estará habilitado para ambas as funções.
+                                        — Tem um cartão com função débito para você fazer compras e saques;<br />
 
-                                    Se você possui a versão antiga do cartão Nubank ou não é cliente do cartão de crédito Nubank, basta solicitar o novo cartão com a função débito - nós emitiremos seu cartão já com a função habilitada.
+                                        — Possibilita pagamento de boletos como conta de luz, água, telefone e até mesmo compras online.<br />
 
-                                    Para isso, basta ir em 'Configurações', clicar em 'Pedir função débito', confirmar o endereço de entrega e pronto! Seu cartão já estará em produção.
+                                    </p>
+
+                                    <p>
+                                        A função débito está disponível para todos os clientes da conta do Nubank! Os clientes interessados em ter um cartão habilitado com essa função devem pedir diretamente pelo nosso app. Abaixo explicamos melhor esse processo.
+
+                                    </p>
+
+                                    <p>
+
+                                        Para quem já possui o novo modelo de cartão de crédito Nubank, lançado em setembro de 2018, basta entrar na área da sua Conta, clicar em 'Pedir função débito' e pronto! Se preferir, pode também arrastar sua tela inicial para cima até a sessão "Descubra Mais" e clicar na opção desejada. Seu cartão já estará habilitado para ambas as funções.
+                                    </p>
+
+                                    <p>
+                                        Se você possui a versão antiga do cartão Nubank ou não é cliente do cartão de crédito Nubank, basta solicitar o novo cartão com a função débito - nós emitiremos seu cartão já com a função habilitada.
+
+                                    </p>
+
+                                    <p>
+                                        Para isso, basta ir em 'Configurações', clicar em 'Pedir função débito', confirmar o endereço de entrega e pronto! Seu cartão já estará em produção.
+
+                                    </p>
                                 </Accordion.Body>
                             </Accordion.Item>
 
                             <Accordion.Item eventKey="2">
                                 <Accordion.Header>Token digital</Accordion.Header>
                                 <Accordion.Body>
-                                    Token é um dispositivo eletrônico gerador de senhas ? que ajuda a manter segurança no app e dispositivo
+                                    Token é um dispositivo eletrônico gerador de senhas que ajuda a manter segurança no app e dispositivo.
                                 </Accordion.Body>
                             </Accordion.Item>
 
